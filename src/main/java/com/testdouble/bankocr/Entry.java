dafc8d700a37b51dd47ae30f0dc434712d0d1748
Glenn Dayton <gdayton@glenns-mbp-2.lan>
2020-03-25 23:14:53 -0700

    added javadocs to entry and entry interface and added more entries test files

--- src/main/java/com/testdouble/bankocr/Entry.java
+++ src/main/java/com/testdouble/bankocr/Entry.java
@@ -3,24 +3,61 @@ package com.testdouble.bankocr;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * Entry contains a List of Digit objects. Contans logic for parsing a 
+ * stringified entry that's 27x4 characters into 9 3 character wide chunks.
+ * 
+ * @author Glenn Dayton
+ * @version 1.0
+ * @since 1.0
+ */
 public class Entry implements EntryInterface
 {
+     /**
+     * Amount of characters wide a digit is.
+     */
     private static final int CHARS_PER_DIGIT = 3;
 
+    /**
+     * Amount of characters wide an entry is.
+     */
     private static final int CHARS_PER_LINE = 27;
 
+    /**
+     * List of parsed Digit objects.
+     */
     private List<Digit> digits;
 
+    /** 
+     * Base constructor for Entry object. Initializes Entry's entries to 
+     * null.
+     */
     public Entry()
     {
         digits = null;
     }
 
+    /**
+     * Constructor for running parsing logic on entry and setting result to
+     * entries.
+     * 
+     * @since 1.0
+     * @param lines A List of 4 lines, each 27 characters long.
+     */
     public Entry(List<String> lines) 
     {
         digits = parseEntry(lines);
     }
 
+    /**
+     * This method takes in a List of Strings each 27 characters wide and 
+     * splits them into 9 3 character wide chunks that will be further parsed
+     * down in the Digit parseDigit method.
+     * 
+     * @since 1.0
+     * @param lines A List of 4 lines, each 27 characters long.
+     * @return A List of parsed Digit objects.
+     */
     public List<Digit> parseEntry(List<String> lines) 
     {
         if(lines.size() == 4) {
@@ -46,6 +83,13 @@ public class Entry implements EntryInterface
         return null;
     }
 
+    /**
+     * This method examines the parsed Digit object and searches for an 
+     * invalid digit, -1. If it occurs, the entry is invalid, false.
+     * 
+     * @since 1.0
+     * @return Returns a true/false boolean value based on if entry contains -1.
+     */
     public boolean isValid() 
     {
         if(digits != null) 
@@ -56,6 +100,14 @@ public class Entry implements EntryInterface
         return false;
     }
 
+    /**
+     * This method iterates through all Digits and calls their toString() 
+     * methods concatenating them into a final Entry string. If the String
+     * contains an invalid Entry "ILL" is appended.
+     * 
+     * @since 1.0
+     * @return String representing Entry
+     */
     public String toString() 
     {
         if(digits != null) {
--- src/main/java/com/testdouble/bankocr/EntryInterface.java
+++ src/main/java/com/testdouble/bankocr/EntryInterface.java
@@ -1,10 +1,27 @@
 package com.testdouble.bankocr;
 
+/**
+ * This interface enforces the Entry class to have the below methods.
+ * 
+ * @author Glenn Dayton
+ * @version 1.0
+ * @since 1.0
+ */
 public interface EntryInterface
 {
-    
+
+    /**
+     * This method determines if an entry contains an invalid integer, -1.
+     * @return Returns a boolean value if Entry is valid or not.
+     * @since 1.0
+     */
     boolean isValid();
 
+    /**
+     * This method returns a Stringified representation of the Entry object.
+     * @return Returns a String value representing an Entry object.
+     * @since 1.0
+     */
     String toString();
 
 }
