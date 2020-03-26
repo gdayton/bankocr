c5a9b53f50b5408cc9c882b4fef43efb5c579a91
Glenn Dayton <gdayton@glenns-mbp-2.lan>
2020-03-25 21:57:05 -0700

    added parsing logic for digit and entry

--- src/main/java/com/testdouble/bankocr/Digit.java
+++ src/main/java/com/testdouble/bankocr/Digit.java
@@ -1,5 +1,7 @@
 package com.testdouble.bankocr;
 
+import java.util.List;
+
 public class Digit implements DigitInterface
 {
     private int digit;
@@ -14,9 +16,45 @@ public class Digit implements DigitInterface
         digit = _digit;
     }
 
+    public Digit(List<String> lines) 
+    {
+        digit = parseDigit(lines);
+    }
+
+    public int parseDigit(List<String> lines) 
+    {
+        String flattened = lines.get(0) + lines.get(1) + lines.get(2);
+
+        switch(flattened) 
+        {
+            case " _ | ||_|": return 0;
+            case "     |  |": return 1;
+            case " _  _||_ ": return 2;
+            case " _  _| _|": return 3;
+            case "   |_|  |": return 4;
+            case " _ |_  _|": return 5;
+            case " _ |_ |_|": return 6;
+            case " _   |  |": return 7;
+            case " _ |_||_|": return 8;
+            case " _ |_| _|": return 9;
+            default: return -1;
+        }
+    }
+
     public int getDigit()
     {
         return this.digit;
+    }
+
+    @Override
+    public boolean equals(Object o) 
+    {
+        if(o instanceof Digit) 
+        {
+            Digit toCompare = (Digit) o;
+            return toCompare.getDigit() == this.digit;
+        }
+        return false;
     }
 
 }
--- src/main/java/com/testdouble/bankocr/Entry.java
+++ src/main/java/com/testdouble/bankocr/Entry.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 public class Entry implements EntryInterface
 {
+    private static final int CHARS_PER_DIGIT = 3;
+
+    private static final int CHARS_PER_LINE = 27;
 
     private List<Digit> digits;
 
@@ -13,8 +16,55 @@ public class Entry implements EntryInterface
         digits = null;
     }
 
-    public String toString()
+    public Entry(List<String> lines) 
     {
+        digits = parseEntry(lines);
+    }
+
+    public List<Digit> parseEntry(List<String> lines) 
+    {
+        if(lines.size() == 4) {
+            digits = new ArrayList<Digit>();
+            List<String> tmpUnparsedDigit = new ArrayList<String>();
+            for(int i = 0; i < CHARS_PER_LINE; i += CHARS_PER_DIGIT) 
+            {
+                int leftBound = i;
+                int rightBound = i + CHARS_PER_DIGIT;
+
+                tmpUnparsedDigit.add(0, lines.get(1).substring(leftBound, rightBound));    // [" _ "]
+                tmpUnparsedDigit.add(1, lines.get(2).substring(leftBound, rightBound));    // [" _|"]
+                tmpUnparsedDigit.add(2, lines.get(3).substring(leftBound, rightBound));    // ["|_ "]
+
+                Digit digit = new Digit(tmpUnparsedDigit);
+                digits.add(digit);
+
+                tmpUnparsedDigit.removeAll(tmpUnparsedDigit);
+            }
+
+            return digits;
+        }
+        return null;
+    }
+
+    public boolean isValid() 
+    {
+        if(digits != null) 
+        {
+            Digit invalidDigit = new Digit(-1);
+            return !digits.contains(invalidDigit);
+        }
+        return false;
+    }
+
+    public String toString() 
+    {
+        if(digits != null) {
+            String s = "";
+            for(Digit digit: digits)
+                s += digit.getDigit() != -1 ? digit.getDigit() : "?";
+            s+= isValid() ? "" : " ILL";
+            return s;
+        }
         return "";
     }
 
