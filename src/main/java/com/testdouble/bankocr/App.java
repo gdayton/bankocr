f2f64eda093f712dd8230fe798b8fc8a1257d49c
Glenn Dayton <gdayton@glenns-mbp-2.lan>
2020-03-25 22:51:07 -0700

    added to toString method and added javadocs to bankocr and app

--- src/main/java/com/testdouble/bankocr/App.java
+++ src/main/java/com/testdouble/bankocr/App.java
@@ -1,13 +1,19 @@
 package com.testdouble.bankocr;
 
 /**
- * Hello world!
- *
+ * Entrypoint for running the entire application. This class calls upon 
+ * the {@link com.testdouble.bankocrkata.BankOcr} singleton class.  
+ * 
+ * @author Glenn Dayton
+ * @version 1.0
+ * @since 1.0
  */
 public class App 
 {
     public static void main( String[] args )
     {
         BankOcr bankOcr = BankOcr.getInstance();
+        bankOcr.readInFile("entries/mixed");
+        System.out.println(bankOcr);
     }
 }
--- src/main/java/com/testdouble/bankocr/BankOcr.java
+++ src/main/java/com/testdouble/bankocr/BankOcr.java
@@ -7,23 +7,61 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
+/**
+ * BankOcr is a singleton that contains all the logic for this 
+ * application. It's the object instantiated in {@link com.testdouble.bankocrkata.App} 
+ * that does all the work in the readInFile method.
+ * 
+ * @author Glenn Dayton
+ * @version 1.0
+ * @since 1.0
+ * 
+ */
 public class BankOcr
 {
+    /**
+     * Amount of rows that an entry is comprized of. This includes
+     * the blank line that appears on top seperating two entries 
+     * from one another. 
+     */
+    private static final int LINES_PER_ENTRY = 4;
 
     private static BankOcr bankOcr = new BankOcr();
 
+    /**
+     * Location where parsed entries reside as collection of 
+     * Entry objects.
+     */
     private static List<Entry> entries;
 
+    /**
+     * Base constructor for BankOcr, initializes BankOcr with 
+     * an empty collection of Entry objects.
+     */
     private BankOcr()
     {
         entries = new ArrayList<Entry>();
     }
 
+    /**
+     * Returns instance of BankOcr
+     * 
+     * @return BankOcr instantiated instance.
+     * @since 1.0
+     */
     public static BankOcr getInstance() 
     {
         return bankOcr;
     }
-
+    
+    /**
+     * Given a path to a file of formatted entries, returns a parsed
+     * collection of Entry objects.
+     * 
+     * @param filePath Relative location to the entries file (e.g. "entries/1")
+     * @return A List of Entry objects that were parsed from a desired file location
+     * @since 1.0
+     */
     public List<Entry> readInFile(String filePath) 
     {
         BufferedReader reader;
@@ -61,13 +99,31 @@ public class BankOcr
         return entries;
     }
 
+    /**
+     * Getter method for List of entries.
+     * 
+     * @return Returns a List of Entry objects.
+     * @since 1.0
+     */
     public List<Entry> entries() {
         return entries;
     }
 
+    /**
+     * toString overriden to display all Entry object's toString outputs
+     * seperated by a newline character.
+     * 
+     * @return A stringified List of Entry objects.
+     * @since 1.0
+     */
     public String toString()
     {
-        return "";
+        String s = "";
+        for(Entry entry: entries) {
+            s += entry;
+            s += "\n";
+        }
+        return s;
     }
 
 }
